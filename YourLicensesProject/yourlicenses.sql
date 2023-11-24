39c2b70ba3add4fc7d916c941ef3147a6f57c5ce
StanOfOld <120013223+StanOfOld@users.noreply.github.com>
2023-11-23 19:28:10 -0500

    Added 2 columns to software and a unique to account email

--- YourLicensesProject/yourlicenses.sql
+++ YourLicensesProject/yourlicenses.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 17, 2023 at 01:06 AM
+-- Generation Time: Nov 24, 2023 at 01:26 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -44,7 +44,8 @@ CREATE TABLE `account` (
 --
 
 INSERT INTO `account` (`accountID`, `firstName`, `lastName`, `email`, `address`, `postalCode`, `password`, `provider`, `authenticationCode`) VALUES
-(1, 'dlo', 'fdjs', 'fjs@gmail.com', '2192 3rd ve', 'yuw4s1', 'defre', 0, 'null');
+(19, 'dlo', 'fdjs', 'fjs@gmail.com', '2192 3rd ve', 'yuw4s1', 'defre', 0, 'null'),
+(20, 'pid', 'fjew', 'daa@gmail.com', 'daudh', 'dsuds', 'fesufb', 0, 'null');
 
 -- --------------------------------------------------------
 
@@ -58,7 +59,8 @@ CREATE TABLE `license` (
   `clientOwnerID` int(11) NOT NULL,
   `serialNum` varchar(250) NOT NULL,
   `purchaseDate` date NOT NULL DEFAULT current_timestamp(),
-  `expiryDate` date DEFAULT NULL
+  `expiryDate` date DEFAULT NULL,
+  `Enabled` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -75,8 +77,27 @@ CREATE TABLE `software` (
   `description` text NOT NULL,
   `numDownloads` int(11) NOT NULL,
   `price` decimal(10,0) NOT NULL,
-  `imageLink` varchar(250) DEFAULT NULL
+  `imageLink` varchar(250) DEFAULT NULL,
+  `imgLinkPr1` varchar(250) DEFAULT NULL,
+  `imgLinkPr2` varchar(250) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `software`
+--
+
+INSERT INTO `software` (`softwareID`, `ownerID`, `name`, `genre`, `description`, `numDownloads`, `price`, `imageLink`, `imgLinkPr1`, `imgLinkPr2`) VALUES
+(8, 19, 'gsdg', 'Graphic', 'Lorem vsdgfs', 3, 4, 'null', NULL, NULL),
+(9, 19, 'Ghfsfu', 'Graphic', 'Lorem vsfdvg', 3, 4, 'null', NULL, NULL),
+(10, 19, 'adsa', 'Graphic', 'Lorem vfsdg', 3, 4, 'null', NULL, NULL),
+(11, 19, 'adsa', 'Utility', 'Lorem Ipsum', 3, 4, 'null', NULL, NULL),
+(12, 19, 'adsa', 'Graphic', 'Lorem Ipsum', 3, 4, 'null', NULL, NULL),
+(13, 19, 'adsa', 'Utility', 'Lorem  gf', 3, 4, 'null', NULL, NULL),
+(14, 19, 'dhdg', 'Graphic', 'Lorem vdster', 3, 4, 'null', NULL, NULL),
+(15, 19, 'hqevs', 'Utility', 'Lorem vsg', 3, 4, 'null', NULL, NULL),
+(16, 19, 'njtr', 'Graphic', 'Lorem svr', 3, 4, 'null', NULL, NULL),
+(17, 19, 'wrtt', 'Utility', 'Lorem yiky', 3, 4, 'null', NULL, NULL),
+(18, 19, 'Ghu', 'Graphic', 'Lorem Ipsum', 3, 4, 'null', NULL, NULL);
 
 --
 -- Indexes for dumped tables
@@ -86,7 +107,8 @@ CREATE TABLE `software` (
 -- Indexes for table `account`
 --
 ALTER TABLE `account`
-  ADD PRIMARY KEY (`accountID`);
+  ADD PRIMARY KEY (`accountID`),
+  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- Indexes for table `license`
@@ -111,19 +133,19 @@ ALTER TABLE `software`
 -- AUTO_INCREMENT for table `account`
 --
 ALTER TABLE `account`
-  MODIFY `accountID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `accountID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT for table `license`
 --
 ALTER TABLE `license`
-  MODIFY `licenseID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `licenseID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `software`
 --
 ALTER TABLE `software`
-  MODIFY `softwareID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `softwareID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- Constraints for dumped tables
